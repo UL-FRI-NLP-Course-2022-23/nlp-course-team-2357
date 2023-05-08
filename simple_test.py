c744cfbc77397db2728d6dbb150f69ec7ac3f50d
lukaboljevic <lukaboljevic.boljevic@gmail.com>
2023-05-08 23:33:23 +0200

    Add different output generation methods

--- simple_test.py
+++ simple_test.py
@@ -4,25 +4,79 @@ from transformers import (
     T5Tokenizer,
 )
 
-model_id = 'Paraphrase_generator'
-model = T5ForConditionalGeneration.from_pretrained(model_id)
+model = T5ForConditionalGeneration.from_pretrained('Paraphrase_generator')
 #model = MT5ForConditionalGeneration.from_pretrained(model_id)
 tokenizer = T5Tokenizer.from_pretrained('cjvt/t5-sl-small')
 
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 model = model.to(device)
 
-def generate(text,model,tokenizer):
-   model.eval()
-   input_ids = tokenizer.encode(text,return_tensors="pt").to(device)  
-   outputs = model.generate(input_ids,max_new_tokens=1000)
-   return tokenizer.decode(outputs[0], skip_special_tokens=True)
+def generate(text, model, tokenizer, method="greedy"):
+    """
+    Feel free to play around with temperature, top_k and top_p values
+    """
+    model.eval()
+    input_ids = tokenizer.encode(text, return_tensors="pt").to(device)
+
+    num_outputs = 3
+    beams = 5
+    temperature = 0.7
+    top_k = 50
+    top_p = 0.95
+
+    if method == "greedy":
+        outputs = model.generate(input_ids, max_new_tokens=1000)
+    elif method == "beam":
+        outputs = model.generate(input_ids,
+                                 max_new_tokens=1000, 
+                                 num_beams=beams,
+                                 early_stopping=True,
+                                 num_return_sequences=min(num_outputs, beams),
+                                 # no_repeat_ngram_size=3,
+                                 )
+    elif method == "sample":
+        outputs = model.generate(input_ids,
+                                 max_new_tokens=1000,
+                                 do_sample=True,
+                                 temperature=temperature,
+                                 top_k=0,
+                                 num_return_sequences=num_outputs,
+                                 )
+    elif method == "top_k":
+        outputs = model.generate(input_ids,
+                                 max_new_tokens=1000,
+                                 do_sample=True,
+                                 temperature=temperature,
+                                 top_k=top_k,
+                                 num_return_sequences=num_outputs,
+                                 )
+    elif method == "top_p":
+        outputs = model.generate(input_ids,
+                                 max_new_tokens=1000,
+                                 do_sample=True,
+                                 temperature=temperature,
+                                 top_p=top_p,
+                                 top_k=top_k,
+                                 num_return_sequences=num_outputs,
+                                 )
+        
+    for i, output in enumerate(outputs):
+        print(f"{i+1}: {tokenizer.decode(output, skip_special_tokens=True)}")
+    print("-" * 100)
+    print()
 
 def main():
-    data = input("Please enter the message:\n")
+    inp = "Choose output generation method (greedy, beam, sample, top_k, top_p)\n"
+    method = input(inp)
+    print()
+
+    inp = "Please enter the message, or 'exit' to exit\n"
+    data = input(inp)
+    print("-" * 100)
+
     while 'exit' != data:
-        print(generate(data,model,tokenizer))
-        data = input("Please enter the message:\n")
+        generate(data, model, tokenizer, method=method)
+        data = input(inp)
 
 
 if __name__ == "__main__":
